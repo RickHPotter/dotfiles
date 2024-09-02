9187d5384e030d9b97c0187cb59b16b1e1172806
RickHPotter <luisfla55@hotmail.com>
2024-09-02 09:09:03 -0300

    fix: oracle on ubuntu 24.04 needs manually installation of libaio1 pkg

--- scripting/oracle.rb
+++ scripting/oracle.rb
@@ -24,6 +24,19 @@ module Scripting
         bash("sudo unzip -qq instantclient-sdk-linux.x64-21.12.0.0.0dbru.zip")
       end
 
+      # In Ubuntu 24.04, the package libaio1 was renamed to libaio1t64 and so on.
+      unless `lsb_release -a | grep "Ubuntu 24`.empty?
+        cd("~/Downloads") do
+          download("http://launchpadlibrarian.net/648013231/libtinfo5_6.4-2_amd64.deb")
+          download("http://launchpadlibrarian.net/648013227/libncurses5_6.4-2_amd64.deb")
+          download("http://launchpadlibrarian.net/646633572/libaio1_0.3.113-4_amd64.deb")
+
+          bash("sudo dpgk -i ./libtinfo5_6.4-2_amd64.deb")
+          bash("sudo dpgk -i ./libncurses5_6.4-2_amd64.deb")
+          bash("sudo dpgk -i ./libaio1_0.3.113-4_amd64.deb")
+        end
+      end
+
       puts "Oracle was successfully installed.".end
     end
   end
